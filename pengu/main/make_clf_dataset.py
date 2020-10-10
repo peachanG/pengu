37c9c544e0c2eab23b87fd54ce177b5b085c3061
peachanG <kenkman0427@gmail.com>
2020-10-10 18:27:32 +0900

    Support undersampling

--- pengu/main/make_clf_dataset.py
+++ pengu/main/make_clf_dataset.py
@@ -1,17 +1,21 @@
+import logging
 import argparse
 from dataclasses import dataclass, asdict
 from pathlib import Path
 from typing import List, Tuple, Optional, Dict
 
 import pandas as pd
+from imblearn.under_sampling import RandomUnderSampler
 
-from pengu.crawler import ImagesDataCSV
+from pengu.data.crawler import ImagesDataCSV
 from pengu.utils.file_io import YamlConfig, save_yaml
+from pengu.utils.function import set_logging_basic_config
 from pengu.dataset.image import ImageTFRecord
 
 
 @dataclass
 class MakeClfDatasetMainConfig(YamlConfig):
+    use_random_under_sampling: bool
     ratio: List[float]
     label2index: Optional[Dict[str, int]] = None
 
@@ -40,9 +44,25 @@ def main():
     config: MakeClfDatasetMainConfig = MakeClfDatasetMainConfig.load(
         args.config_path, process_name="make_clf_dataset")
 
-    # preprocess image data csv
-    df: pd.DataFrame = ImagesDataCSV.create_df_dropped_hash_duplicates(args.input_img_data_csv)
+    set_logging_basic_config("INFO")
+
+    # Drop hash duplicates & file_path is Null
+    df: pd.DataFrame = ImagesDataCSV.create_df_with_duplicate_hashes_removed(
+        args.input_img_data_csv)
     df: pd.DataFrame = df.dropna(subset=[ImagesDataCSV.FILE_PATH_COL])
+    counts_after_duplicates_nan: pd.Series = df.value_counts(ImagesDataCSV.LABEL_COL)
+    logging.info(f"Counts after dropping row with null {ImagesDataCSV.FILE_PATH_COL}!")
+    logging.info(counts_after_duplicates_nan)
+
+    # Under Sampling
+    if config.use_random_under_sampling:
+        rus: RandomUnderSampler = RandomUnderSampler(random_state=0)
+        df, _ = rus.fit_resample(df, df[ImagesDataCSV.LABEL_COL])  # type: ignore
+        counts_after_undersampling: pd.Series = df.value_counts(ImagesDataCSV.LABEL_COL)
+        logging.info("Counts after under sampling!")
+        logging.info(counts_after_undersampling)
+
+    # Split data
     ratio: Tuple[float, float, float] = tuple(config.ratio)  # type: ignore
     train_df, val_df, test_df = ImagesDataCSV.split_train_val_test(
         df=df, ratio=ratio)
@@ -50,6 +70,13 @@ def main():
     train_csv_path = args.output_preprocessed_csv_dir / "train.csv"
     val_csv_path = args.output_preprocessed_csv_dir / "val.csv"
     test_csv_path = args.output_preprocessed_csv_dir / "test.csv"
+    logging.info("Split data into train and val, test!")
+    logging.info("Counts Train")
+    logging.info(train_df.value_counts(ImagesDataCSV.LABEL_COL))
+    logging.info("Counts Validation")
+    logging.info(val_df.value_counts(ImagesDataCSV.LABEL_COL))
+    logging.info("Counts Test")
+    logging.info(test_df.value_counts(ImagesDataCSV.LABEL_COL))
     train_df.to_csv(train_csv_path, index=False)
     val_df.to_csv(val_csv_path, index=False)
     test_df.to_csv(test_csv_path, index=False)
